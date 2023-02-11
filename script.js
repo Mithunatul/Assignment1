1107240ec67170f8b36459995cad8143dc23fd01
Mithunatul <108508574+Mithunatul@users.noreply.github.com>
2023-02-11 09:48:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//Welcome
+// alert("Welcome");
+//Console
+// let myText =
+//   "Welcome to my officail Portfolio Website Where I put all my work related to my designs and many more.";
+// console.log(myText);
